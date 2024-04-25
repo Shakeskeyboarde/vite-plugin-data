70267411d0c7dac83be19488342269e906b46dad
Chris Ackerman <chris@topher.land>
2024-04-25 14:20:33 -0700

    chore: add toJSON support demo

--- packages/demo/src/message.data.ts
+++ packages/demo/src/message.data.ts
@@ -8,6 +8,20 @@ import url from 'node:url';
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 const text = await fs.readFile(path.resolve(__dirname, 'message.json'), 'utf8');
 
+class Foo {
+  text = 'foo text';
+
+  toJSON(): {} {
+    return { text: this.text };
+  }
+}
+
+/**
+ * Should not cause an error even though a class instance is exported, because
+ * the class has a toJSON method which returns a JSON-safe value.
+ */
+export const foo = new Foo();
+
 /**
  * Message from the local JSON file.
  */
