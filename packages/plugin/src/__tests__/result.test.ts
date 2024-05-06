15c2a0fac2fc2bd24c17559a5851e38885532575
FreeJ1nG <Andrewjeremy12345@gmail.com>
2024-05-06 21:51:28 +0700

    chore: add unit tests for result class

--- packages/plugin/src/__tests__/result.test.ts
+++ packages/plugin/src/__tests__/result.test.ts
@@ -1,5 +1,29 @@
-import { test } from 'vitest';
+import { assert, test } from "vitest";
 
-test.todo('constructor sets properties correctly');
-test.todo('dependsOn correctly matches dependencies');
-test.todo('dependsOn correctly matches dependency patterns');
+import { MOCK_RESULT_PARAM } from "../__mocks__/result.mock.js";
+import { Result } from "../result.js";
+
+test("constructor sets properties correctly", () => {
+  const result = new Result(MOCK_RESULT_PARAM);
+  assert.deepEqual(result.dependencies, MOCK_RESULT_PARAM.dependencies);
+  assert.deepEqual(result.exports, MOCK_RESULT_PARAM.exports);
+  assert.deepEqual(
+    result.dependencyPatterns,
+    MOCK_RESULT_PARAM.dependencyPatterns,
+  );
+});
+
+test("dependsOn correctly matches dependencies", () => {
+  const result = new Result(MOCK_RESULT_PARAM);
+  assert.isTrue(result.dependsOn("/root/dummy-dependency-1.ts"));
+  assert.isTrue(result.dependsOn("/root/dummy-dependency-2.ts"));
+});
+
+test("dependsOn correctly matches dependency patterns", () => {
+  const result = new Result(MOCK_RESULT_PARAM);
+  assert.isTrue(result.dependsOn("/root/folder/somedata.json"));
+  assert.isTrue(result.dependsOn("/root/data.txt"));
+  assert.isTrue(
+    result.dependsOn("/root/data/some/path/to/folder/somefile.txt"),
+  );
+});
