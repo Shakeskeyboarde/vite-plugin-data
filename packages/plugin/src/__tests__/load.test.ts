23177ceb849916fb94ce4db4311934157b600cca
Chris Ackerman <chris@topher.land>
2024-05-06 17:01:38 -0700

    ci: add load tests

--- packages/plugin/src/__tests__/load.test.ts
+++ packages/plugin/src/__tests__/load.test.ts
@@ -1,6 +1,46 @@
-import { test } from 'vitest';
+import path from 'node:path';
 
-test.todo('bundles and imports a data loader module');
-test.todo('correctly detects dependencies');
-test.todo('correctly detects dependency patterns');
-test.todo('injects expected module constants like import.meta.url');
+import { normalizePath } from 'vite';
+import { expect, test } from 'vitest';
+
+import { load } from '../load.js';
+
+test('bundles and imports a data loader module', async () => {
+  const { exports } = await load(path.resolve(__dirname, 'src/demo.data.ts'));
+
+  expect(exports.default).toBe('default-string');
+  expect(exports.imported).toBe('imported-string');
+  expect(exports.named).toBe('named-string');
+  expect(exports.promised).toBeInstanceOf(Promise);
+  await expect(exports.promised).resolves.toBe('promised-string');
+  expect(exports.read).toBeInstanceOf(Promise);
+  await expect(exports.read).resolves.toBe('read-string');
+});
+
+test('correctly detects dependencies', async () => {
+  const { dependencies } = await load(path.resolve(__dirname, 'src/demo.data.ts'));
+
+  expect(dependencies).toStrictEqual([
+    normalizePath(path.resolve(__dirname, 'src/demo.data.ts')),
+    normalizePath(path.resolve(__dirname, 'src/to-be-imported.ts')),
+  ]);
+});
+
+test('correctly detects dependency patterns', async () => {
+  const { dependencyPatterns } = await load(path.resolve(__dirname, 'src/demo.data.ts'));
+
+  expect(dependencyPatterns).toStrictEqual([
+    normalizePath(path.resolve(__dirname, 'src/data.txt')),
+  ]);
+});
+
+test('injects expected module constants', async () => {
+  const result = await load(path.resolve(__dirname, 'src/meta.data.ts'));
+  const exports: Record<string, any> = result.exports;
+
+  expect(exports.default.url).toBe('file://' + normalizePath(path.resolve(__dirname, 'src/meta.data.ts')));
+  expect(exports.default.filename).toBe(normalizePath(path.resolve(__dirname, 'src/meta.data.ts')));
+  expect(exports.default.dirname).toBe(normalizePath(path.resolve(__dirname, 'src')));
+  expect(exports.default.__filename).toBe(normalizePath(path.resolve(__dirname, 'src/meta.data.ts')));
+  expect(exports.default.__dirname).toBe(normalizePath(path.resolve(__dirname, 'src')));
+});
---
+++ packages/plugin/src/__tests__/src/meta.data.ts
@@ -0,0 +1,7 @@
+export default {
+  url: import.meta.url,
+  dirname: import.meta.dirname,
+  filename: import.meta.filename,
+  __dirname,
+  __filename,
+};
